12848b94476b33cb3e9a69d518be694bc398483c
Kendra Kyndberg <kendrak922@gmail.com>
2018-09-11 10:23:19 -0500

    modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -3,15 +3,36 @@ import firebase from '../../firebase';
 
 
 class Modal extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
             name: '',
             email: '',
-            clients: []
+            clients: [],
+            showModal:false,
+            modal:{
+                position: 'absolute',
+                top: '50%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)',
+                zIndex:'9999',
+            },
+            background: {
+                position: 'fixed',
+                width: '100%',
+                height: '100%',
+                top: '0px',
+                left: '0px',
+                zIndex:'9998'
+
+            }
         }
+    
+
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+
 
     }
     handleChange(e) {
@@ -33,16 +54,31 @@ class Modal extends Component {
             email: ''
         });
     }
+    handleClick(){
+        this.setState(prevState=>({
+            showModal: !prevState.showModal
+        }));
+    }
 
     render() {
+        const modal = (
+            <div className="modal-background" style={this.state.background}>
+            <div className='modal-style' style={this.state.modal}>
+            <div className="modal-title"></div>
+            <div className="modal-btn" onClick={this.handleClick}>X</div>
+            <form onSubmit={this.handleSubmit}>
+                <input type="text" name="name" onChange={this.handleChange} value={this.state.name} placeholder="Young Bill" />
+                <input type="text" name="email" onChange={this.handleChange} value={this.state.email} placeholder="HadToBeYou@gmail.com" />
+                <button onSubmit={this.handleSubmit}>Submit</button>
+            </form>
+        </div>
+        </div>
+        )
         return (
-            <section className='add-item'>
-                <form onSubmit={this.handleSubmit}>
-                    <input type="text" name="name" onChange={this.handleChange} value={this.state.name} placeholder="Young Bill" />
-                    <input type="text" name="email" onChange={this.handleChange} value={this.state.email} placeholder="HadToBeYou@gmail.com" />
-                    <button onSubmit={this.handleSubmit}>Submit</button>
-                </form>
-            </section>
+        <div className="contact">
+       <button className="button" onClick={this.handleClick}>Let's Talk</button>
+       {this.state.showModal ? modal : ''}
+        </div>
         )
     }
 }
